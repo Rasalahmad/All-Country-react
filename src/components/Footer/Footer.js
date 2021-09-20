48f75e39486280525aec39b8f3edba8931528722
Rasalahmad <rasalahmadsit@gmail.com>
2021-09-20 13:09:09 +0600

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div>
+            <p className = 'footer'>Copyright &copy; projected</p>
+        </div>
+    );
+};
+
+export default Footer;
